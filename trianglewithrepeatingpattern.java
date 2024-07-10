532d5a23bef8707bb1bd00cc28c53f40cb2c23bd
Nikilesh Kumar <92156967+NIKILESH58@users.noreply.github.com>
2024-07-11 00:06:03 +0530

    Add files via upload

---
+++ trianglewithrepeatingpattern.java
@@ -0,0 +1,20 @@
+public class TrianglewithRepeatingPattern
+{
+    public static void main(String[] args)
+    {
+        int n =9;
+         for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                                    System.out.print(j);
+            }
+             for(int k=i-1;k>0;k--)
+            {
+                                    System.out.print(k);
+            }
+                                    System.out.println(" ");
+        }
+        
+    }
+}
